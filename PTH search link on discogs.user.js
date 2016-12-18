5a8edc83a97ec997fffb58a1d4d7537782303971
Chameleon <neo.chameleon@gmail.com>
2016-12-18 10:53:29 +1000

    Added torrent search

--- PTH search link on discogs.user.js
+++ PTH search link on discogs.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         PTH search link on discogs
-// @version      0.1
+// @version      0.2
 // @description  Link to a search from discogs pages to PTH
 // @author       Chameleon
 // @include      http*://*discogs.com/*
@@ -13,6 +13,9 @@
   var pTitle=document.getElementById('profile_title');
   if(pTitle)
   {
+    var album = pTitle.getElementsByTagName('span');
+    album=album[album.length-1].textContent.trim();
+    
     var span=pTitle.getElementsByTagName('a')[0].parentNode;
     var artist=span.getAttribute('title');
     
@@ -22,5 +25,12 @@
     span.appendChild(document.createTextNode(' ('));
     span.appendChild(a);
     span.appendChild(document.createTextNode(')'));
+        
+    var a=document.createElement('a');
+    a.href="https://passtheheadphones.me/torrents.php?searchstr="+encodeURIComponent(artist+' '+album);
+    a.innerHTML = "pth";
+    pTitle.appendChild(document.createTextNode(' ('));
+    pTitle.appendChild(a);
+    pTitle.appendChild(document.createTextNode(')'));
   }
 })();
