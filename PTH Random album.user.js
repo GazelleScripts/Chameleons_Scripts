cf64a67d5fcc1c25772b98b5ddef782b3518c7bc
Chameleon <neo.chameleon@gmail.com>
2016-12-18 13:54:29 +1000

    Added Random album

---
+++ PTH Random album.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         PTH Random album
+// @version      0.1
+// @description  Load a random album
+// @author       Chameleon
+// @include      http*://passtheheadphones.me/torrents.php?id=*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+  'use strict';
+
+  var linkbox=document.getElementsByClassName('linkbox')[0];
+  var a=document.createElement('a');
+  linkbox.appendChild(a);
+  a.innerHTML = 'Random album';
+  a.setAttribute('class', 'brackets');
+  a.href='/torrents.php?id='+Math.round(Math.random()*265246);
+})();
