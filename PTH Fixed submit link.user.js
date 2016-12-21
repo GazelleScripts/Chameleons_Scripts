4aebfa31e23e8d2fe0c0679d1e2a68b5a0753ef9
Chameleon <neo.chameleon@gmail.com>
2016-12-21 14:54:20 +1000

    Fixed z-index bug on some stylesheets

--- PTH Fixed submit link.user.js
+++ PTH Fixed submit link.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         PTH Fixed submit link
-// @version      0.1
+// @version      0.2
 // @description  Add a link that can submit the upload form from anywhere on the page
 // @author       Chameleon
 // @include      http*://passtheheadphones.me/upload.php*
@@ -13,7 +13,7 @@
   var a=document.createElement('a');
   document.body.appendChild(a);
   a.href='javascript:void(0);';
-  a.setAttribute('style', 'display: block; padding: 10px; border-radius: 10px; background: rgba(0,0,0,0.7); color: white; position: fixed; bottom: 10px; margin: auto; left: 0; right: 0; text-align: center; width: 100px; border: rgba(255,255,255,0.3) solid 2px;');
+  a.setAttribute('style', 'z-index: 1000; display: block; padding: 10px; border-radius: 10px; background: rgba(0,0,0,0.7); color: white; position: fixed; bottom: 10px; margin: auto; left: 0; right: 0; text-align: center; width: 100px; border: rgba(255,255,255,0.3) solid 2px;');
   a.innerHTML = 'Upload torrents';
   a.addEventListener('click', uploadTorrents, false);
 })();
