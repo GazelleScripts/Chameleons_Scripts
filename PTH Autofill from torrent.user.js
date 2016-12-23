f92b7f5d2c59bd93d7bb663d6364caaa0b26a55f
Chameleon <neo.chameleon@gmail.com>
2016-12-24 02:25:14 +1000

    Added format and bitrate detection on extra formats

--- PTH Autofill from torrent.user.js
+++ PTH Autofill from torrent.user.js
@@ -1,15 +1,16 @@
 // ==UserScript==
 // @name         PTH Autofill from torrent
-// @version      1.0
+// @version      1.1
 // @description  Attempt to read the .torrent file and fill in the artist and album
 // @author       Chameleon
 // @include      http*://passtheheadphones.me/upload.php*
 // @grant        none
+// @run-at       document-idle
 // ==/UserScript==
 
 (function() {
   'use strict';
-  
+
   var a=document.createElement('a');
   a.innerHTML = 'Auto-fill from torrent: On';
   if(window.location.href.indexOf('groupid=') != -1)
@@ -20,9 +21,66 @@
   var before=document.getElementById('upload_table');
   before.parentNode.insertBefore(a, before);
 
-  document.getElementById('file').addEventListener('change', fileAdded.bind(undefined, a), false);
-  
+  var format=document.getElementById('format');
+  var bitrate=document.getElementById('bitrate');
+
+  document.getElementById('file').addEventListener('change', fileAdded.bind(undefined, a, format, bitrate, true), false);
+
+  addToExisting(a);
+
+  // select the target node
+  var target = document.getElementById('upload_table');
+
+  // create an observer instance
+  var observer = new MutationObserver(addedToTable.bind(undefined, a));
+
+  // configuration of the observer:
+  var config = { childList: true, subtree: true };
+
+  // pass in the target node, as well as the observer options
+  observer.observe(target, config);
+
 })();
+
+function addToExisting(a)
+{
+  var tr=document.getElementById('extra_format_row_1');
+  var index=1;
+  while(tr)
+  {
+    var input=tr.getElementsByTagName('input')[0];
+    var format=tr.getElementsByTagName('select');
+    var bitrate=format[1];
+    format=format[0];
+
+    input.addEventListener('change', fileAdded.bind(undefined, a, format, bitrate, false), false);
+    
+    index++;
+    tr=document.getElementById('extra_format_row_'+index);
+  }
+}
+
+function addedToTable(a, mutations)
+{
+  for(var i=0; i<mutations.length; i++)
+  {
+    var m=mutations[i];
+    if(m.addedNodes.length == 1)
+    {
+      var id=m.addedNodes[0].getAttribute('id');
+      if(id && id.indexOf('extra_format_row') != -1)
+      {
+        var tr=m.addedNodes[0];
+        var input=tr.getElementsByTagName('input')[0];
+        var format=tr.getElementsByTagName('select');
+        var bitrate=format[1];
+        format=format[0];
+
+        input.addEventListener('change', fileAdded.bind(undefined, a, format, bitrate, false), false);
+      }
+    }
+  }
+}
 
 function toggle(a)
 {
@@ -36,53 +94,53 @@ function toggle(a)
   }
 }
 
-function fileAdded(a, event)
+function fileAdded(a, format, bitrate, full, event)
 {
   if(a.innerHTML.indexOf('Off') != -1)
     return;
   var file=event.target.files[0];
   if(!file)
     return;
-  
-  var spl=file.name.replace(/ - [0-9][0-9][0-9][0-9]/, '').split(' - ');
-  if(spl.length == 1)
+
+  if(full)
   {
-    var fn=file.name;
-    var length=4;
-    var search=fn.search(/[0-9][0-9][0-9][0-9]/);
-    if(search != -1)
+    var spl=file.name.replace(/ - [0-9][0-9][0-9][0-9]/, '').split(' - ');
+    if(spl.length == 1)
     {
-      if(fn[search-1] == '(')
+      var fn=file.name;
+      var length=4;
+      var search=fn.search(/[0-9][0-9][0-9][0-9]/);
+      if(search != -1)
       {
-        search--;
-        length=6;
+        if(fn[search-1] == '(')
+        {
+          search--;
+          length=6;
+        }
+        spl=[];
+        spl.push(fn.substring(0, search).trim());
+        spl.push(fn.substring(search+length).trim());
       }
-      spl=[];
-      spl.push(fn.substring(0, search).trim());
-      spl.push(fn.substring(search+length).trim());
+    }
+    var artist=spl[0].split('] ');
+    if(artist.length > 1)
+      artist=artist[1];
+    else
+      artist=artist[0];
+    document.getElementById('artist').value=artist;
+    var album=spl[1].split(' (')[0].split(' [')[0].split(' {')[0].replace(/.torrent$/, '');
+    document.getElementById('title').value=album;
+
+    var yadg=document.getElementById('yadg_input');
+    if(yadg)
+    {
+      yadg.value=artist+' '+album;
+      document.getElementById('yadg_submit').click();
     }
   }
-  var artist=spl[0].split('] ');
-  if(artist.length > 1)
-    artist=artist[1];
-  else
-    artist=artist[0];
-  document.getElementById('artist').value=artist;
-  var album=spl[1].split(' (')[0].split(' [')[0].split(' {')[0].replace(/.torrent$/, '');
-  document.getElementById('title').value=album;
-  
-  var yadg=document.getElementById('yadg_input');
-  if(yadg)
-  {
-    yadg.value=artist+' '+album;
-    document.getElementById('yadg_submit').click();
-  }
-  
+
   var f=file.name.toLowerCase();
-  
-  var format=document.getElementById('format');
-  var bitrate=document.getElementById('bitrate');
-  
+
   if(f.indexOf('mp3') != -1)
   {
     format.selectedIndex=1;
@@ -104,7 +162,7 @@ function fileAdded(a, event)
   {
     format.selectedIndex=5;
   }
-  
+
   if(f.indexOf('v0') != -1)
   {
     bitrate.selectedIndex=7;
@@ -135,7 +193,7 @@ function fileAdded(a, event)
     bitrate.selectedIndex=8;
     format.selectedIndex=1;
   }
-  
+
   var media=document.getElementById('media');
   if(f.indexOf('dvd') != -1)
   {
@@ -171,11 +229,11 @@ function fileAdded(a, event)
   }
   else
     media.selectedIndex=1;
-  
+
   triggerChange(format);
   triggerChange(bitrate);
   triggerChange(media);
-  
+
   var r=new FileReader();
   r.onload=readFile;
   r.readAsText(file);
@@ -233,7 +291,7 @@ function readFile(event)
     finalTracks.push(t.track);
   }
   finalTracks.sort();
-  
+
   desc.value='Track list:';
   for(var i=0; i<finalTracks.length; i++)
   {
