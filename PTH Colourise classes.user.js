0c999ed3ceb4f712fa688e3ee9e70a3301c17ecd
Chameleon <neo.chameleon@gmail.com>
2018-02-28 16:40:47 +1000

    Added staff to colourised classes

--- PTH Colourise classes.user.js
+++ PTH Colourise classes.user.js
@@ -17,6 +17,14 @@
                ['(Torrent Master)', '#FFFFAA'],
                ['(Power TM)', '#FFFF66'],
                ['(Elite TM)', '#FFFF00'],
+               ['(Forum Mod)', 'rgba(0,0,0,1); text-shadow:rgba(255,255,255,1) 0px 0px 3px;'],
+               ['(Moderator)', 'rgba(0,0,0,1); text-shadow:rgba(128,255,255,1) 0px 0px 3px;'],
+               ['(Senior Moderator)', 'rgba(0,0,0,1); text-shadow:rgba(255,128,255,1) 0px 0px 3px;'],
+               ['(Developer)', 'rgba(0,0,0,1); text-shadow:rgba(255,255,128,1) 0px 0px 3px;'],
+               ['(Administrator)', 'rgba(0,0,0,1); text-shadow:rgba(255,192,0,1) 0px 0px 3px;'],
+               ['(Sysop)', 'rgba(0,0,0,1); text-shadow:rgba(255,0,0,1) 0px 0px 3px;'],
+               ['(VIP)', 'rgba(0,0,0,1); text-shadow:rgba(0,192,255,1) 0px 0px 3px;'],
+               ['(Legend)', 'rgba(0,0,0,0.2); text-shadow:rgba(128,128,128,1) 0px 0px 1px;'],
     ];
 
   var strongs=document.getElementsByTagName('strong');
@@ -26,7 +34,7 @@
     for(var j=0; j<classes.length; j++)
     {
       if(s.innerHTML == classes[j][0])
-        s.style.color=classes[j][1];
+        s.setAttribute('style', 'color:'+classes[j][1]);
     }
   }
 })();
